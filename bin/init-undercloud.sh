3b24f532660ce5738074f8bbcd015cc8b808ca48
Martin André <m.andre@redhat.com>
2016-11-10 22:53:00 +0100

    More undercloud init

--- bin/init-undercloud.sh
+++ bin/init-undercloud.sh
@@ -19,27 +19,34 @@ enable_ui = true\
 ' undercloud.conf
     # sudo pip install -U --no-deps ./instack-undercloud/
     # sudo pip install -U --no-deps ./tripleo-validations/
-    sudo pip install -U --no-deps ./tripleo-common/
-
-    # Until tripleo-common rpm is updated
-    #sudo curl https://raw.githubusercontent.com/openstack/mistral/87aeb0ac5afb9765ed81fe48264f14f35ea9dedc/mistral/engine/rpc.py -o /usr/lib/python2.7/site-packages/mistral/engine/rpc.py
+    # sudo pip install -U --no-deps ./tripleo-common/
 
 elif [ $PROJECT == 'containers' ]; then
     sudo yum install -y docker
     sudo systemctl enable docker
     sudo systemctl start docker
+    sudo sed -i 's/5000/8787/' /etc/sysconfig/docker-registry
     sudo systemctl enable docker-registry
     sudo systemctl start docker-registry
     sudo pip install docker-py
+    sudo chmod 0666 /run/docker.sock
 
     # Install kolla in case we need to rebuild images
-    # git clone https://github.com/openstack/kolla.git
-    # cd kolla
-    # git checkout stable/mitaka
-    # virtualenv ~/kolla-venv
-    # source ~/kolla-venv/bin/activate
-    # pip install -U pip
-    # pip install -r requirements.txt
+    git clone https://github.com/openstack/kolla.git
+    cd kolla
+    git checkout stable/mitaka
+    virtualenv ~/kolla-venv
+    source ~/kolla-venv/bin/activate
+    pip install -U pip
+    pip install -r requirements.txt
+    time ./tools/build.py --base centos --type binary --namespace tripleoupstream --registry localhost:8787 --tag mitaka --push \
+	neutron-openvswitch-agent \
+	nova-compute \
+	nova-libvirt \
+	openvswitch-db-server \
+	openvswitch-vswitchd
+    cd
+    deactivate
 fi
 
 ./undercloud-install.sh
@@ -61,14 +68,23 @@ if [ $PROJECT == 'containers' ]; then
     openstack image create --file CentOS-Atomic-Host-7-GenericCloud.qcow2 --disk-format qcow2 --container-format bare atomic-image
 
     # Deploy latest artifacts
-    # ./pull_puppet_modules.sh
-    # tripleo-common/scripts/upload-puppet-modules --directory ~/puppet-modules/
+    ./pull_puppet_modules.sh
+    tripleo-common/scripts/upload-puppet-modules --directory ~/puppet-modules/
 
     # Populate docker registry
-    sudo python /home/stack/tripleo-common/upload_shit.py
-    sudo /home/stack/tripleo-common/rebuild-heat-agents.sh
-    sudo docker tag localhost:8787/tripleoupstream/centos-binary-data:liberty localhost:8787/tripleoupstream/centos-binary-data:mitaka
-    sudo docker push localhost:8787/tripleoupstream/centos-binary-data:mitaka
+    # sudo python /home/stack/tripleo-common/upload_shit.py
+    /home/stack/tripleo-common/rebuild-heat-agents.sh
+
+    # Raise heat nested stack limit
+    sudo sed -i -r "s,^[# ]*max_nested_stack_depth *=.+$,max_nested_stack_depth = 6," /etc/heat/heat.conf
+    sudo systemctl restart openstack-heat-engine
+    
+    # Setup network isolation
+    cat >/home/stack/custom.yaml <<-EOF
+parameter_defaults:
+  EC2MetadataIp: 192.0.2.1
+  ControlPlaneDefaultRoute: 192.0.2.1
+EOF
 fi
 
 if [ $PROJECT == 'validations' ]; then
@@ -80,5 +96,5 @@ elif [ $PROJECT == 'containers' ]; then
     # source stackrc
     # openstack stack delete --yes --wait overcloud
     # mistral environment delete overcloud
-    # openstack overcloud deploy --templates /home/stack/tripleo-heat-templates/ -e /home/stack/tripleo-heat-templates/environments/docker.yaml -e /home/stack/tripleo-heat-templates/environments/docker-network.yaml --libvirt-type qemu
+    # openstack overcloud deploy --templates /home/stack/tripleo-heat-templates/ -e /home/stack/tripleo-heat-templates/environments/docker.yaml -e /home/stack/tripleo-heat-templates/environments/docker-network.yaml -e /home/stack/tripleo-heat-templates/environments/docker-network-isolation.yaml -e /home/stack/tripleo-heat-templates/environments/network-isolation.yaml -e /home/stack/tripleo-heat-templates/environments/net-single-nic-with-vlans.yaml -e /home/stack/custom.yaml --libvirt-type qemu
 fi

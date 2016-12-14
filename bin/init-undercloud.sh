10836e95bb794540b5c33f3ad7a098b18cab059a
Martin André <m.andre@redhat.com>
2016-12-14 09:49:16 +0100

    Refactor init-undercloud.sh

--- bin/init-undercloud.sh
+++ bin/init-undercloud.sh
@@ -2,41 +2,70 @@
 
 set -e
 
-PROJECT=$1
-if [ -z $PROJECT ]; then
-    # PROJECT='validations'
-    PROJECT='containers'
-fi
+: ${OPT_REBUILD_IMAGES:=0}
 
-sudo pip install -U pip
+usage() {
+    echo "Usage: $0 [options]"
+    echo ""
+    echo "Options:"
+    echo "  --rebuild-images    rebuild Kolla and heat-agents images"
+    echo "  -h, --help          print this help and exit"
+}
 
-if [ $PROJECT == 'validations' ]; then
-    sed -i '3i\
-enable_validations = true\
-' undercloud.conf
-    sed -i '3i\
-enable_ui = true\
-' undercloud.conf
-    # sudo pip install -U --no-deps ./instack-undercloud/
-    # sudo pip install -U --no-deps ./tripleo-validations/
-    # sudo pip install -U --no-deps ./tripleo-common/
+while [ "x$1" != "x" ]; do
 
-elif [ $PROJECT == 'containers' ]; then
-    sudo yum install -y docker
-    sudo groupadd docker
-    sudo gpasswd -a ${USER} docker
-    newgrp docker
-    sudo systemctl enable docker
-    sudo systemctl start docker
-    sudo sed -i 's/5000/8787/' /etc/sysconfig/docker-registry
-    sudo systemctl enable docker-registry
-    sudo systemctl start docker-registry
-    sudo pip install docker-py
+    case "$1" in
+        --rebuild-images)
+            OPT_REBUILD_IMAGES=1
+            ;;
 
-    # NOTE(flaper87): This should not longer be needed
-    # since we're creating the docker group
-    sudo chmod 0666 /run/docker.sock
+        --help|-h)
+            usage
+            exit
+            ;;
 
+        --) shift
+            break
+            ;;
+
+        -*) echo "ERROR: unknown option: $1" >&2
+            usage >&2
+            exit 2
+            ;;
+
+        *)  break
+            ;;
+    esac
+
+    shift
+done
+
+install_undercloud() {
+    if [ ! -f ~/stackrc ]; then
+	./undercloud-install.sh
+	./overcloud-prep-images.sh
+	./overcloud-prep-flavors.sh
+	./overcloud-prep-network.sh
+	./overcloud-custom-tht-script.sh
+    fi
+}
+
+raise_heat_nested_stack_limit() {
+    # Raise heat nested stack limit
+    sudo sed -i -r "s,^[# ]*max_nested_stack_depth *=.+$,max_nested_stack_depth = 6," /etc/heat/heat.conf
+    sudo systemctl restart openstack-heat-engine
+}
+
+allow_non_root_docker() {
+    if ! getent group docker >/dev/null; then
+	sudo groupadd docker
+	sudo gpasswd -a ${USER} docker
+	newgrp docker
+	sudo systemctl restart docker
+    fi
+}
+
+build_kolla_images() {
     # Install kolla in case we need to rebuild images
     git clone https://github.com/openstack/kolla.git
     cd kolla
@@ -54,54 +83,66 @@ elif [ $PROJECT == 'containers' ]; then
 	openvswitch-vswitchd
     cd
     deactivate
-fi
+}
 
-./undercloud-install.sh
-./overcloud-prep-images.sh
-./overcloud-prep-flavors.sh
-./overcloud-prep-network.sh
-./overcloud-custom-tht-script.sh
+populate_docker_registry() {
+    if [ "$OPT_REBUILD_IMAGES" = 1 ]; then
+	build_kolla_images
+	/home/stack/tripleo-common/rebuild-heat-agents.sh
+    else
+	python -c "from tripleo_common.image.image_uploader import ImageUploadManager; ImageUploadManager(['/home/stack/tripleo-common/contrib/overcloud_containers.yaml']).upload()"
+    fi
+}
 
-source ~/stackrc
-# Configure nameserver for the overcloud
-SUBNET_UUID=$(neutron subnet-list -c id -f value)
-neutron subnet-update ${SUBNET_UUID} --dns-nameserver 192.168.23.1
-
-if [ $PROJECT == 'containers' ]; then
-
+pull_atomic_host() {
     # Upload atomic-host image to glance
     wget http://cloud.centos.org/centos/7/atomic/images/CentOS-Atomic-Host-7-GenericCloud.qcow2.gz
     gunzip CentOS-Atomic-Host-7-GenericCloud.qcow2.gz
     openstack image create --file CentOS-Atomic-Host-7-GenericCloud.qcow2 --disk-format qcow2 --container-format bare atomic-image
+}
 
+# Configure nameserver for the overcloud
+configure_overcloud_dns() {
+    SUBNET_UUID=$(neutron subnet-list -c id -f value)
+    neutron subnet-update ${SUBNET_UUID} --dns-nameserver 192.168.23.1
+}
+
+deploy_latest_puppet_modules() {
     # Deploy latest artifacts
     ./pull_puppet_modules.sh
     tripleo-common/scripts/upload-puppet-modules --directory ~/puppet-modules/
+}
 
-    # Populate docker registry
-    # sudo python /home/stack/tripleo-common/upload_shit.py
-    /home/stack/tripleo-common/rebuild-heat-agents.sh
-
-    # Raise heat nested stack limit
-    sudo sed -i -r "s,^[# ]*max_nested_stack_depth *=.+$,max_nested_stack_depth = 6," /etc/heat/heat.conf
-    sudo systemctl restart openstack-heat-engine
-    
-    # Setup network isolation
+# FIXME(mandre) should pick the IP address of the undercloud instead
+# it has changed to 192.168.24.1 in recent undercloud
+setup_network_isolation() {
     cat >/home/stack/custom.yaml <<-EOF
 parameter_defaults:
   EC2MetadataIp: 192.0.2.1
   ControlPlaneDefaultRoute: 192.0.2.1
 EOF
-fi
+}
 
-if [ $PROJECT == 'validations' ]; then
-    # ./overcloud-deploy.sh
-    # ./overcloud-deploy-post.sh
-    echo
-elif [ $PROJECT == 'containers' ]; then
-    echo "skipping overcloud install"
-    # source stackrc
-    # openstack stack delete --yes --wait overcloud
-    # mistral environment delete overcloud
-    # openstack overcloud deploy --templates /home/stack/tripleo-heat-templates/ -e /home/stack/tripleo-heat-templates/environments/docker.yaml -e /home/stack/tripleo-heat-templates/environments/docker-network.yaml -e /home/stack/tripleo-heat-templates/environments/docker-network-isolation.yaml -e /home/stack/tripleo-heat-templates/environments/network-isolation.yaml -e /home/stack/tripleo-heat-templates/environments/net-single-nic-with-vlans.yaml -e /home/stack/custom.yaml --libvirt-type qemu
-fi
+deploy_overcloud_normal() {
+    ./overcloud-deploy.sh
+    ./overcloud-deploy-post.sh
+}
+
+deploy_overcloud_containers() {
+    source stackrc
+    openstack stack delete --yes --wait overcloud
+    mistral environment delete overcloud
+    openstack overcloud deploy --templates /home/stack/tripleo-heat-templates/ -e /home/stack/tripleo-heat-templates/environments/docker.yaml -e /home/stack/tripleo-heat-templates/environments/docker-network.yaml -e /home/stack/tripleo-heat-templates/environments/docker-network-isolation.yaml -e /home/stack/tripleo-heat-templates/environments/network-isolation.yaml -e /home/stack/tripleo-heat-templates/environments/net-single-nic-with-vlans.yaml -e /home/stack/custom.yaml --libvirt-type qemu
+}
+
+
+install_undercloud
+raise_heat_nested_stack_limit
+allow_non_root_docker
+populate_docker_registry
+
+source ~/stackrc
+pull_atomic_host
+configure_overcloud_dns
+deploy_latest_puppet_modules
+#deploy_overcloud_containers

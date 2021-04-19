162f58c0cf34bcf3f66abb5e9f52e10acd496dde
Martin André <martin.andre@gmail.com>
2021-04-19 17:17:16 +0200

    Add bash-language-server

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -48,7 +48,7 @@ end
 
 -- Use a loop to conveniently both setup defined servers 
 -- and map buffer local keybindings when the language server attaches
-local servers = { "pyright", "gopls" }
+local servers = { "pyright", "gopls", "bashls" }
 for _, lsp in ipairs(servers) do
   nvim_lsp[lsp].setup { on_attach = require'completion'.on_attach }
 end

cc46ffb454293b2182a9f8deb0e9d2eb92f8fe3c
Martin André <martin.andre@gmail.com>
2021-07-14 18:40:01 +0200

    Adjust colors for LspReference

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -34,9 +34,9 @@ local on_attach = function(client, bufnr)
   -- Set autocommands conditional on server_capabilities
   if client.resolved_capabilities.document_highlight then
     vim.api.nvim_exec([[
-      hi LspReferenceRead cterm=bold ctermbg=red guibg=LightYellow
-      hi LspReferenceText cterm=bold ctermbg=red guibg=LightYellow
-      hi LspReferenceWrite cterm=bold ctermbg=red guibg=LightYellow
+      hi LspReferenceRead cterm=bold gui=bold ctermbg=237 guibg=#343d46
+      hi LspReferenceText cterm=bold gui=bold ctermbg=237 guibg=#343d46
+      hi LspReferenceWrite cterm=bold gui=bold ctermbg=237 guibg=#343d46
       augroup lsp_document_highlight
         autocmd! * <buffer>
         autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()

4701c452d22ee0de5041f3a16e14ab57e04f6958
Martin André <martin.andre@gmail.com>
2021-10-31 20:44:49 +0100

    vim: organize go imports on write
    Following docs at https://github.com/golang/tools/blob/master/gopls/doc/vim.md#neovim-imports

--- .config/nvim/init-0.5.vim
+++ .config/nvim/init-0.5.vim
@@ -213,6 +213,7 @@ if has("autocmd")
   augroup filetype_go
     autocmd!
     autocmd BufWritePre *.go lua vim.lsp.buf.formatting_sync(nil, 1000)
+    autocmd BufWritePre *.go lua goimports(1000)
   augroup END
   " }}}
 
--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -52,8 +52,40 @@ local servers = { "pyright", "gopls", "bashls" }
 for _, lsp in ipairs(servers) do
   nvim_lsp[lsp].setup {
     on_attach = on_attach,
+    -- To use LSP snippets
+    -- capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
     flags = {
       debounce_text_changes = 150,
     }
   }
+end
+
+function goimports(timeoutms)
+  local context = { source = { organizeImports = true } }
+  vim.validate { context = { context, "t", true } }
+
+  local params = vim.lsp.util.make_range_params()
+  params.context = context
+
+  -- See the implementation of the textDocument/codeAction callback
+  -- (lua/vim/lsp/handler.lua) for how to do this properly.
+  local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, timeout_ms)
+  if not result or next(result) == nil then return end
+  local actions = result[1].result
+  if not actions then return end
+  local action = actions[1]
+
+  -- textDocument/codeAction can return either Command[] or CodeAction[]. If it
+  -- is a CodeAction, it can have either an edit, a command or both. Edits
+  -- should be executed first.
+  if action.edit or type(action.command) == "table" then
+    if action.edit then
+      vim.lsp.util.apply_workspace_edit(action.edit)
+    end
+    if type(action.command) == "table" then
+      vim.lsp.buf.execute_command(action.command)
+    end
+  else
+    vim.lsp.buf.execute_command(action)
+  end
 end

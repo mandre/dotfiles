03fc42ce825e8a2783adea785ce94aa6cba955d0
Martin André <m.andre@redhat.com>
2022-10-24 16:49:04 +0200

    vim: LSP client.resolved_capabilities -> client.server_capabilities

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -26,14 +26,14 @@ local on_attach = function(client, bufnr)
   buf_set_keymap('n', '<space>q', '<cmd>lua vim.diagnostic.set_loclist()<CR>', opts)
 
   -- Set some keybinds conditional on server capabilities
-  if client.resolved_capabilities.document_formatting then
+  if client.server_capabilities.document_formatting then
     buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
-  elseif client.resolved_capabilities.document_range_formatting then
+  elseif client.server_capabilities.document_range_formatting then
     buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.range_formatting()<CR>", opts)
   end
 
   -- Set autocommands conditional on server_capabilities
-  if client.resolved_capabilities.document_highlight then
+  if client.server_capabilities.document_highlight then
     vim.api.nvim_exec([[
       hi LspReferenceRead cterm=bold gui=bold ctermbg=237 guibg=#343d46
       hi LspReferenceText cterm=bold gui=bold ctermbg=237 guibg=#343d46
